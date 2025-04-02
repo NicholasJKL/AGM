a2d25f4e13fe80325a413b06a10267ac18406b7c
NicholasJKL <matveev_andrey2003@mail.ru>
2025-04-02 13:58:28 +0300

    New Formula

--- AGM/PreciseCalc.cs
+++ AGM/PreciseCalc.cs
@@ -3,60 +3,64 @@ using System.Diagnostics;
 
 namespace AGM
 {
-	public static class PreciseCalc
-	{
-		public static double AGM(double a, double b, int precision = 4)
-		{
-			while (Math.Abs(a - b) > Math.Pow(10, -(precision)))
-			{
-				double temp = a;
+    public static class PreciseCalc
+    {
+        public static double AGM(double a, double b, int precision = 8)
+        {
+            while (Math.Abs(a - b) > Math.Pow(10, -(precision)))
+            {
+                double temp = a;
 
-				temp = (a + b) / 2;
-				b = Math.Sqrt(a * b);
-				a = temp;
+                temp = (a + b) / 2;
+                b = Math.Sqrt(a * b);
+                a = temp;
 
-				// Debug.WriteLine($"a - {a}");
-				// Debug.WriteLine($"b - {b}");
-			}
+                // Debug.WriteLine($"a - {a}");
+                // Debug.WriteLine($"b - {b}");
+            }
 
-			return a;
-		}
+            return a;
+        }
 
-		public static double MAGM(double a, double b, int precision = 4) // Переделать
-		{
-			double c = 0;
+        public static double MAGM(double a, double b, int precision = 4) // Переделать
+        {
+            double c = 0;
 
-			while (Math.Abs(a - b) > Math.Pow(10, -(precision)))
-			{
-				a = (a + b) / 2;
-				b = c + Math.Sqrt((a - c) * (b - c));
-				c = c - Math.Sqrt((a - c) * (b - c));
+            while (Math.Abs(a - b) > Math.Pow(10, -(precision)))
+            {
+                a = (a + b) / 2;
+                b = c + Math.Sqrt((a - c) * (b - c));
+                c = c - Math.Sqrt((a - c) * (b - c));
 
-				// Debug.WriteLine($"a - {a}");
-				// Debug.WriteLine($"b - {b}");
-			}
+                // Debug.WriteLine($"a - {a}");
+                // Debug.WriteLine($"b - {b}");
+            }
 
-			return a;
-		}
+            return a;
+        }
 
-		public static double SympsonWaveEq(double r, double t, double R)
-		{
-			double h = r / 6,
-				term1 = (4 * Math.Exp(-r/2) * r/2) / AGM(Math.Sqrt(Math.Pow(t, 2) - Math.Pow(R - (r / 2), 2)), Math.Sqrt(Math.Pow(t, 2) - Math.Pow(R + (r / 2), 2))),
-				term2 = (Math.Exp(-r) * r) / AGM(Math.Sqrt(Math.Pow(t, 2) - Math.Pow(R - r, 2)), Math.Sqrt(Math.Pow(t, 2) - Math.Pow(R + r, 2)));
+        public static double SympsonWaveEq(double r, double t, double R)
+        {
+            if (t < R + r)
+            {
+                return 0;
+            }
 
-			Debug.WriteLine(term1);
-			Debug.WriteLine(term2);
-			Debug.WriteLine(h * (term1 + term2));
+            double h = r / 6,
+                term1 = (4 * Math.Exp(-r / 2) * (r / 2)) / AGM(Math.Sqrt(Math.Pow(t, 2) - Math.Pow(R - (r / 2), 2)), Math.Sqrt(Math.Pow(t, 2) - Math.Pow(R + (r / 2), 2))),
+                term2 = (Math.Exp(-r) * r) / AGM(Math.Sqrt(Math.Pow(t, 2) - Math.Pow(R - r, 2)), Math.Sqrt(Math.Pow(t, 2) - Math.Pow(R + r, 2)));
 
-			return h * (term1 + term2);
-		}
+            Debug.Print($"term1 = {term1}");
+            Debug.Print($"term2 = {term2}");
 
-		public static double ExpWaveEq(double r)
-		{
-			double res = -((r - Math.Exp(r) + 1) / (Math.Exp(r)));
+            return h * (term1 + term2);
+        }
 
-			return res;
-		}
-	}
+        public static double ExpWaveEq(double r)
+        {
+            double res = -((r - Math.Exp(r) + 1) / (Math.Exp(r)));
+
+            return res;
+        }
+    }
 }
--- AGM/WaveEq.xaml.cs
+++ AGM/WaveEq.xaml.cs
@@ -5,38 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace AGM
 {
-	/// <summary>
-	/// Interaction logic for WaveEq.xaml
-	/// </summary>
-	public partial class WaveEq : Page
-	{
-		public WaveEq()
-		{
-			InitializeComponent();
+    /// <summary>
+    /// Interaction logic for WaveEq.xaml
+    /// </summary>
+    public partial class WaveEq : Page
+    {
+        public WaveEq()
+        {
+            InitializeComponent();
 
-			var dataX = new List<double>();
-			var dataY = new List<double>();
+            var dataX = new List<double>();
+            var dataY = new List<double>();
 
-			double t = 1, R = 0.5, r0 = 0.01;
+            double t = 1;
 
-			for (double i = 0; i <= R; i += 0.01)
-			{
-				dataX.Add(i);
+            for (double R = 0; R <= t; R += 0.01)
+            {
+                double r0 = t - R - 0.01;
 
-				dataY.Add(PreciseCalc.SympsonWaveEq(r0, t, i));
-			}
+                dataX.Add(R);
 
-			WavePlot.Plot.Add.Scatter(dataX, dataY);
-		}
-	}
+                dataY.Add(PreciseCalc.SympsonWaveEq(r0, t, R));
+            }
+
+            WavePlot.Plot.Add.Scatter(dataX, dataY);
+        }
+    }
 }
